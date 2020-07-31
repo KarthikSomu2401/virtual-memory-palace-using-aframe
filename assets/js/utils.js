2e019d3ee8402fbf5ac167cd6a6b401668f6fc0c
Karthikeyan Somasundaram <karthikitmsp@gmail.com>
2020-07-31 14:43:45 +0100

    result display

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -6,3 +6,21 @@ AFRAME.registerComponent("pickup-object", {
     });
   },
 });
+
+AFRAME.registerComponent("prepare-info", {
+  init: function () {
+    var element = this.el;
+    element.addEventListener("click", function (evt) {
+      document.querySelector("#resultobject").setAttribute("visible", true);
+    });
+  },
+});
+
+AFRAME.registerComponent("display-recipe", {
+  init: function () {
+    var element = this.el;
+    element.addEventListener("click", function (evt) {
+      document.querySelector("#displayfinal").setAttribute("visible", true);
+    });
+  },
+});
